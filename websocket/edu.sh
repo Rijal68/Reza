f4537ed51179cd020b5a482ce0df30bbed104396
Rijal68 <111652181+Rijal68@users.noreply.github.com>
2022-08-29 14:00:34 +0700

    Update edu.sh

--- websocket/edu.sh
+++ websocket/edu.sh
@@ -89,7 +89,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-tls 443
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-tls 700
 Restart=on-failure
 
 [Install]
